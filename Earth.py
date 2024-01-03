c4c10065d90082d5240c7d003890aacf4ecbc421
Cyber-boy <135902087+Root-acess@users.noreply.github.com>
2024-01-03 09:48:28 -0800

    Earth.py
    3d model of earth

---
+++ Earth.py
@@ -0,0 +1,9 @@
+from vpython import *
+from time import *
+
+space = canvas(width= 1280 , height= 720)
+earth = sphere(pos = vector(0,0,0), radius = 1, texture = textures.earth)
+rotation_speed = 0.02
+while True :
+    rate(120)
+    earth.rotate(angle = rotation_speed ,axis = vector(0,1,0))
